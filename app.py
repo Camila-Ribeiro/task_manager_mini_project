8dfa8971cdb0bc21ba256ac73577152a63658e58
Camila-Ribeiro <camilarf@gmail.com>
2020-06-12 14:07:09 +0000

    added collapsible jQuery

--- app.py
+++ app.py
@@ -7,10 +7,8 @@ if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
-
 app.config['MONGO_DBNAME'] = os.environ.get('SECRET_KEY')
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
-
 app.secret_key = os.environ.get('SECRET_KEY')
 
 mongo = PyMongo(app)
