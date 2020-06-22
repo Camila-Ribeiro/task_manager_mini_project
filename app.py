5fc352be320ba22ff6c1916ccca3c1545b94c297
Camila-Ribeiro <camilarf@gmail.com>
2020-06-22 12:08:57 +0000

    added code to app.py

--- app.py
+++ app.py
@@ -7,16 +7,13 @@ if os.path.exists('env.py'):
     import env
 
 app = Flask(__name__)
-# app.config['MONGO_DBNAME'] = os.environ.get('SECRET_KEY')
-# app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
-# app.secret_key = os.environ.get('SECRET_KEY')
-os.environ['SECRET_KEY'] = 'USerRooT'
+app.config['MONGO_DBNAME'] = os.environ.get('SECRET_KEY')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+app.secret_key = os.environ.get('SECRET_KEY')
 
-os.environ['MONGO_DBNAME'] = 'task_manager'
-os.environ['MONGO_URI'] = os.getenv('MONGO_URI', 'mongodb+srv://root:USerRooT@myfirstcluster-cpqdb.mongodb.net/task_manager?retryWrites=true&w=majority')
 mongo = PyMongo(app)
 
-# @app.route('/')
+@app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
     return render_template('tasks.html', tasks=mongo.db.tasks.find())
